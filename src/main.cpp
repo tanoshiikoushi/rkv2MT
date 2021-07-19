c0fe6177f3ac0aba82e96074388a07051a9274da
tanoshiikoushi <68407204+tanoshiikoushi@users.noreply.github.com>
2021-07-18 22:04:25 -0600

    submodule update and better testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
 #include "RKV2.h"
+#include <stdio.h>
 
 int main(int argc, char* argv[])
 {
-	std::cout << "Hello :D\n";
-	test_func();
+	u32 checksum = test_func();
+	printf("0x%.8X\n", checksum);
 	return 0;
 }
