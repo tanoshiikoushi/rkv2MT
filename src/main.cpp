626cd00b31c87701fed15a1a40e23543ce3ee97e
tanoshiikoushi <68407204+tanoshiikoushi@users.noreply.github.com>
2021-07-24 22:00:39 -0600

    first functional release

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,49 @@
-#include "RKV2.h"
 #include <stdio.h>
+#include <fstream>
+#include "RKV2.h"
 
 int main(int argc, char* argv[])
 {
-	u32 checksum = test_func();
-	printf("0x%.8X\n", checksum);
-	return 0;
+    if (argc != 3) {
+        printf("Invalid usage!\n");
+        printf("Execute as: rkv2MT \"C:\\dir\\to\\file.rkv\" \"C:\\dir\\to\\output\\to\"\n");
+        return 0;
+    }
+
+    std::string file_name(argv[1]);
+    std::string out_path(argv[2]);
+
+    std::fstream file_in;
+    u64 file_size;
+
+    file_in.open(file_name, std::ios_base::in | std::ios_base::binary);
+
+    if (!file_in) {
+        printf("Unable to open file");
+        return 1;
+    }
+
+    file_in.seekg(0, std::ios_base::end);
+    file_size = file_in.tellg();
+
+    file_in.seekg(0, std::ios_base::beg);
+
+    u8* in_data = new u8[file_size];
+
+    file_in.read((char*)in_data, file_size);
+    file_in.close();
+
+    RKV2File* rkv = new RKV2File();
+    if (rkv->load(in_data, file_size)) {
+        if(rkv->extract((u8*)out_path.c_str())) {
+            printf("success!\n");
+        } else {
+            printf("failure :(\n");
+        }
+    }
+
+    delete in_data;
+    delete rkv;
+
+    return 0;
 }
